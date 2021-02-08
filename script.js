94ace515a4f6a95a146785e70d7f473ebd704c5a
bksingh2000 <73107544+bksingh2000@users.noreply.github.com>
2021-02-09 02:10:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,14 +2,10 @@ const app = document.getElementById('root')
 const container = document.createElement('div')
 container.setAttribute('class', 'container')
 app.appendChild(container)
-    // Create a request variable and assign a new XMLHttpRequest object to it.
 var request = new XMLHttpRequest()
-
-// Open a new connection, using the GET request on the URL endpoint
 request.open('GET', 'https://blogsadda.herokuapp.com/api/', true)
 
 request.onload = function() {
-    // Begin accessing JSON data here
     var data = JSON.parse(this.response)
     if (request.status >= 200 && request.status < 400) {
         data.forEach((blog) => {
