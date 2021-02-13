575f4078ed8eb9d54e130d5bd82837c39c5536c9
bksingh2000 <73107544+bksingh2000@users.noreply.github.com>
2021-02-13 15:24:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,31 @@
+function capitalizeFirstLetter(string) {
+    // return string && string[0].toUpperCase() + string.slice(1); //for empty string
+    return string[0].toUpperCase() + string.slice(1);
+}
+
+function isValidEmailAddress(emailAddress) {
+    var pattern = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
+    return pattern.test(emailAddress);
+}
 const app = document.getElementById('root')
 
 var request = new XMLHttpRequest()
-var url = 'https://blogsadda.herokuapp.com/api/'
-request.open('GET', url, true)
+var link = 'https://blogsadda.herokuapp.com/api/'
+request.open('GET', link, true)
 
 request.onload = function() {
     var data = JSON.parse(this.response)
     if (request.status >= 200 && request.status < 400) {
-        data.forEach((blog) => {
+        data.results.forEach((blog) => {
             const main = document.createElement('div')
             main.setAttribute('class', 'card')
-                // main.setAttribute("data-identity", blog.id)
 
             const title = document.createElement('h2')
             title.setAttribute('class', 't-capital')
             title.textContent = blog.title
 
             const author = document.createElement('h5')
-            author.setAttribute('class', 'capital')
-            author.textContent = blog.owner + " on " + blog.created_at
+            author.textContent = "Puplished by " + capitalizeFirstLetter(blog.owner) + " on " + blog.created_at
 
             const image = document.createElement('div')
             image.setAttribute('class', 'img')
