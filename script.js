01a999f7176b107ec56eeee434eae99b54b06992
bksingh2000 <73107544+bksingh2000@users.noreply.github.com>
2021-02-16 01:28:24 +0530

    updated

--- script.js
+++ script.js
@@ -1,30 +1,5 @@
-function capitalizeFirstLetter(string) {
-    return string[0].toUpperCase() + string.slice(1);
-}
-
-function isValidEmailAddress(emailAddress) {
-    var pattern = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
-    return pattern.test(emailAddress);
-}
-var link = 'https://blogsadda.herokuapp.com/api/'
-
 const root = document.getElementById('root')
-var loader = document.getElementById('loader')
-var modal = document.getElementById('myModal');
-var span = document.getElementsByClassName("close")[0];
-var title = document.getElementById("title");
-var details = document.getElementById("details");
-var author = document.getElementById("author");
-var owner = document.getElementById("owner");
-var alert = document.getElementById("alert");
-span.onclick = function() {
-    modal.style.display = "none";
-}
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
+var err = document.getElementById("alert");
 
 function allBlogs() {
     $.ajax({
@@ -36,7 +11,7 @@ function allBlogs() {
         },
         success: function(data) {
             $.each(data.results, function(k, v) {
-                root.innerHTML += "<div class='card'><h2 class='t-capital'>" + v.title + "</h2><h5 class='author'>" + "Puplished by " + capitalizeFirstLetter(v.owner) + " on " + v.created_at + "</h5><div class='img' style='background:url(" + v.img + ");background-position: center;background-size: cover;'></div><p class='capital content'>" + v.content.substring(0, 300) + "...<a href='#!' data-identity='" + v.id + "'>Read More</a></p></div>"
+                root.innerHTML += "<div class='w3-card-4 w3-margin w3-white'><img src='' style='width:100%;'><div class='w3-container'><h3><b>" + v.title + "</b></h3><h5>" + capitalizeFirstLetter(v.owner) + ", <span class='w3-opacity'>" + v.created_at + "</span></h5></div><div class='w3-container'><p>" + v.content.substring(0, 300) + "...</p><div class='w3-row'><div class='w3-col m8 s12'><p><form action='blog.html' method='GET'><input type='hidden' name='id' value='" + v.id + "'><button class='w3-button w3-padding-large w3-white w3-border'><b>READ MORE »</b></button></form></p></div><div class='w3-col m4 w3-hide-small'><p><span class='w3-padding-large w3-right'><b>Comments  </b> <span class='w3-tag'>" + v.comment + "</span></span></p></div></div></div></div><hr>"
             })
         },
         complete: function() {
@@ -44,27 +19,13 @@ function allBlogs() {
         },
         error: function(XMLHttpRequest, textStatus, errorThrown) {
             if (XMLHttpRequest.readyState == 4) {
-                root.innerHTML = "<div class='card'><p>" + XMLHttpRequest.status + " " + XMLHttpRequest.statusText + "</p></div>"
+                root.innerHTML = "<div class='w3-card-4 w3-margin w3-white'><div class='w3-container'><h3><b>" + XMLHttpRequest.status + " " + XMLHttpRequest.statusText + "</b></h3></div></div><hr>"
             } else if (XMLHttpRequest.readyState == 0) {
-                root.innerHTML = "<div class='card'><p>Network Error!!</p></div>"
+                root.innerHTML = "<div class='w3-card-4 w3-margin w3-white'><div class='w3-container'><h3><b>Network error!!</b></h3></div></div><hr>"
             } else {
                 console.log("something weird is happening")
             }
         }
-    })
-}
-
-function findById(id) {
-    $.ajax({
-        type: 'GET',
-        url: link + id + '/',
-        dataType: 'json',
-        success: function(data) {
-            title.innerText = data.title
-            details.innerText = data.content
-            owner.innerText = "Puplished by " + capitalizeFirstLetter(data.owner) + " on " + data.created_at
-            modal.style.display = "block"
-        },
     })
 }
 
@@ -81,16 +42,16 @@ function create_post() {
             $('#name').val('');
             $('#email').val('');
             $('#msg').val('');
-            alert.innerHTML = "<span class='closebtn'>&times;</span>Message sent successfully!"
-            alert.style.backgroundColor = "#4CAF50"
-            alert.style.display = "block";
+            err.innerHTML = "<span class='closebtn'>&times;</span>Message sent successfully!"
+            err.style.backgroundColor = "#4CAF50"
+            err.style.display = "block";
         },
 
         error: function(XMLHttpRequest, textStatus, errorThrown) {
             if (XMLHttpRequest.readyState == 4) {
                 console.log(XMLHttpRequest.status)
             } else if (XMLHttpRequest.readyState == 0) {
-                alert.innerText = "<span class='closebtn'>&times;</span>Network Error"
+                err.innerText = "<span class='closebtn'>&times;</span>Network Error"
             } else {
                 console.log("something weird is happening")
             }
@@ -100,9 +61,6 @@ function create_post() {
 
 $(document).ready(function() {
     allBlogs()
-    $('#root').on('click', '.card .content a', function() {
-        findById($(this).data('identity'));
-    })
     $('#alert').on('click', '.closebtn', function() {
         $('#alert').fadeOut('fast')
     })
@@ -111,13 +69,13 @@ $(document).ready(function() {
         e = $('#email').val().trim();
         m = $('#msg').val().trim();
         if (n == '' || e == '' || m == '') {
-            alert.innerHTML = "<span class='closebtn'>&times;</span>Fill all the details"
-            alert.style.backgroundColor = "#ff9800"
-            alert.style.display = "block"
+            err.innerHTML = "<span class='closebtn'>&times;</span>Fill all the details"
+            err.style.backgroundColor = "#ff9800"
+            err.style.display = "block"
         } else if (!isValidEmailAddress(e)) {
-            alert.innerHTML = "<span class='closebtn'>&times;</span>Email is not valid"
-            alert.style.backgroundColor = "#ff9800"
-            alert.style.display = "block"
+            err.innerHTML = "<span class='closebtn'>&times;</span>Email is not valid"
+            err.style.backgroundColor = "#ff9800"
+            err.style.display = "block"
         } else {
             create_post()
         }
